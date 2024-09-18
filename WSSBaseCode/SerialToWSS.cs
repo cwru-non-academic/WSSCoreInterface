a33c9be8d9d5eae63932005ced25a8033cf8ce9b
lemfn64 <lemfn64@gmail.com>
2024-09-18 18:28:22 -0400

    Update SerialToWSS.cs
    fixed stop command error current feature is pending full testing before merge to main

--- WSSBaseCode/SerialToWSS.cs
+++ WSSBaseCode/SerialToWSS.cs
@@ -211,7 +211,7 @@ public class SerialToWSS
             {
                 started = true;
                 return "Start Acknowleged";
-            } else if (data[4] == 0x04) //stop acknowlege
+            } else if (data[4] == 0x00) //stop acknowlege
             {
                 started= false;
                 return "Stop Acknowleged";
@@ -251,7 +251,7 @@ public class SerialToWSS
 
     private void queueWriteToWSS(byte msgID, byte[] message, int lenght)
     {
-        if((msgID >0x2F && msgID<0x34) || (msgID==0x0B && message[4]==0x04)) //stream msges do not go into queue because they have no response
+        if((msgID >0x2F && msgID<0x34)) //stream msges do not go into queue because they have no response
         {
             WriteToWSS(message, lenght);
         } else
